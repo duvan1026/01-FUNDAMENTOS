205138f92efe6b4d9ef26e8aa3686b5a8a2db201
duvan1026 <duvancastro1026@gmail.com>
2022-12-22 13:33:25 -0500

    app.js: insertamos estilos ccs en consola

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 
 let a = 10,
     b = 20,
-    c = 10,
-    d = 10,
+    c = 30,
+    d = 40,
     x = a + b;
 
-
-console.log( x );
+console.log('%c Mis variables', 'color:blue; font-weight: bold')
+console.log({ a });
+console.log({ b });
+console.log({ c });
