aeddc2d8681a9d25cfb85767d6ff31bd50449142
duvan1026 <duvancastro1026@gmail.com>
2022-12-22 14:47:14 -0500

    app.js: actualizado con manejo de string y variables constantes

--- app.js
+++ app.js
@@ -3,11 +3,15 @@
 
 let a = 10,
     b = 20,
-    c = 30,
-    d = 40,
+    c = 'Hola ',
+    d = 'Spiderman',
     x = a + b;
 
-console.log('%c Mis variables', 'color:blue; font-weight: bold')
-console.log({ a });
-console.log({ b });
-console.log({ c });
+const saludo = c + d;
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold')
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+// console.table({a, b, c, d, x });
