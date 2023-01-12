4cab2694495ffa64f495b12c7462ecbc87323fbe
duvan castro <duvancastro1026@gmail.com>
2023-01-12 05:06:54 -0500

    arreglos.js: Ejemplo arreglo con multiples tipos de datos

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -7,6 +7,21 @@
 
 let videoJuegos = [ 'Maria 3', 'Megaman', 'Chrono Trigger' ];
 console.log( { videoJuegos } );
-
+// Obtener valor de una posición en especifica del arreglo.
 console.log( videoJuegos[0] );
+
+
+// Arreglo de multiple tipos de datos
+let arregloCosas = [
+    true,
+    123,
+    'Duvan',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', 'Dr. Light'],
+];
+
+console.log({ arregloCosas });
 
