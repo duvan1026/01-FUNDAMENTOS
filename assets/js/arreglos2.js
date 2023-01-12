ad1c46a6d2fc656993955f3e9b3d9b6bf0f8be00
duvan1026 <duvancastro1026@gmail.com>
2023-01-12 16:25:07 -0500

    arreglos2.js: Se realiza ejemplo del metodo .splice() para array en JS

--- assets/js/arreglos2.js
+++ assets/js/arreglos2.js
@@ -7,17 +7,28 @@ let ultimo = juegos[ juegos.length - 1 ]; // Calculo para saber cual es el ultim
 
 console.log({ primero, ultimo });
 
-
+// Recorre el arreglo
 juegos.forEach( (elemento, indice, arr) => {
     console.log(elemento,indice,arr)
 } );
 
 
+// Se agrega un elemento al arreglo
 let nuevaLongitud = juegos.push( 'F-Zero' );
 console.log({ nuevaLongitud, juegos });
 
+// Se agrega un elemento al inicio del arreglo
 nuevaLongitud = juegos.unshift('Fire Emblem');
 console.log({ nuevaLongitud, juegos });
 
+// Se elimina el ultimo elemento del arreglo.
 let juegoBorrado = juegos.pop();
 console.log({ juegoBorrado, juegos});
+
+// Borramos un valor en especifico del arreglo
+let pos = 1;
+
+console.log( juegos );
+let juegosBorrados = juegos.splice( pos, 2 ); // en este caso borramos 2 datos apartir de la posicion pos(1) del arreglo y alamacenamos estos dos valores en la varrible asignada.
+console.log({ juegosBorrados, juegos });
+
