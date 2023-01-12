5b6eb1ecca4701eb0ef2b31e1178786ec085309a
duvan castro <duvancastro1026@gmail.com>
2023-01-12 05:00:17 -0500

    arreglos.js: declaración arreglo y navegación en posición especifica

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -1,5 +1,12 @@
 
 
-// const arr = new Array(10); // Poco común su implementación.
-const arr = [];  // Es mas comun verla implementada.
-console.log(arr);
+// // const arr = new Array(10); // Poco común su implementación.
+// const arr = [];  // Es mas comun verla implementada.
+// console.log(arr);
+
+
+let videoJuegos = [ 'Maria 3', 'Megaman', 'Chrono Trigger' ];
+console.log( { videoJuegos } );
+
+console.log( videoJuegos[0] );
+
