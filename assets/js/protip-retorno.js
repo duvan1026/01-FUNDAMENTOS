a3bf0cdfee77d83e2d1cbfc4be6661549504669e
duvan1026 <duvancastro1026@gmail.com>
2023-01-16 13:35:51 -0500

    protip-retorno.js: Implementación proptip para destructuración de argumentos en funciones en JS

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -34,3 +34,38 @@ console.log({ casado, vivo, nombre, saludo });
 // almacenamos el valor apellido en la variable nuevoApellido
 const { apellido: nuevoApellido } = crearPersona( 'Duvan', 'Castro' );
 console.log( { nuevoApellido } );
+
+
+// Destructuración de argumentos.
+
+
+let tony = {
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster',],
+}; // Si tiene {} es un objeto
+
+
+// const imprimePropiedades = ( personaje ) => {
+//     console.log( 'nombre: ', personaje.nombre );
+//     console.log( 'codeName: ', personaje.codeName );
+//     console.log( 'vivo: ', personaje.vivo );
+//     console.log( 'edad: ', personaje.edad );
+//     console.log( 'trajes: ', personaje.trajes );
+
+// }
+
+const imprimePropiedades = ({ nombre, codeName, vivo, edad = 15, trajes }) => {
+
+    // edad = edad || 0; // Validación de dato, si no tiene valor se le asigan un valor de 0
+
+    console.log({nombre});
+    console.log({codeName});    
+    console.log({vivo});
+    console.log({edad});
+    console.log({trajes});    
+}
+
+imprimePropiedades( tony );
