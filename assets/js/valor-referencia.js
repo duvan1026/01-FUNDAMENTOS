a20a99004695460ce66cb6e53e6ce3a4fc0005f6
duvan1026 <duvancastro1026@gmail.com>
2023-01-16 15:55:23 -0500

    valor-referencia.js: se implementa ejemplos de romper la referencia entre objetos en JS

--- assets/js/valor-referencia.js
+++ assets/js/valor-referencia.js
@@ -30,3 +30,23 @@ let tony = cambiaNombre( peter );
 
 
 console.log({ peter, tony });
+
+
+
+// Arreglos.
+
+const frutas = ['Manzana', 'Pera', 'Piña'];
+
+// const otrasFrutas = [ ...frutas ];// Operador SPREAD, rompe la refrencias entre objetos en JS
+
+console.time('slice');//  medir el rendimiento en JS ---- inicio de tiempo
+const otrasFrutas = frutas.slice();// metodo slice() en arreglos, rompe la refrencias entre objetos en JS
+console.timeEnd('slice');// fin de tiempo
+
+console.time('spread');// medir el rendimiento en JS ---- inicio de tiempo
+const otrasFrutas2 = [...frutas];// metodo slice() en arreglos, rompe la refrencias entre objetos en JS
+console.timeEnd('spread');// fin de tiempo
+
+otrasFrutas.push('Mango');
+
+console.log({ frutas, otrasFrutas });
