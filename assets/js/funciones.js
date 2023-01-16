c4928f6eece42c9ba2271caba505ce4e4a572102
duvan1026 <duvancastro1026@gmail.com>
2023-01-16 10:45:32 -0500

    funciones.js: Ejemplo de funciones de flecha con return simplificada en JS

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,10 +1,11 @@
 // Función tradicional
 function saludar( nombre ) {
-    console.log( arguments );
-    console.log('Hola ' + nombre);
-    return 10;
+    // console.log( arguments );
+    // console.log('Hola ' + nombre);
+    return [1,2,3,4];
 
-    console.log('Soy un codifo que está despúes del return'); // esta linea no se ejecutará
+    // esta linea no se ejecutará
+    console.log('Soy un codifo que está despúes del return'); 
 };
 
 // Funcion anonima - indica que la función respondera al nombre de "saludar2"
@@ -25,8 +26,26 @@ const saludarFlecha2 = ( nombre )  => {
 }
 
 const retornoSaludar = saludar( "Duvan", 40, true, 'Costa Rica' ); // 1
-console.log({ retornoSaludar });
+// console.log( retornoSaludar );
 // saludar2( "Duvan Castro" );
 
-saludarFlecha();
-saludarFlecha2( 'Melissa' );
+// saludarFlecha();
+// saludarFlecha2( 'Melissa' );
+
+function sumar( a,b ) {
+    return a + b;
+}
+
+
+// const sumar2 = ( a,b ) => {
+//     return a + b;
+// }
+// Reemplaza la función sumar2 comentada, 
+// solo se aplica si solo la función retorna y no tiene mas lineas de codigo.
+const sumar2 = ( a,b ) => a + b;
+
+
+
+
+console.log( sumar(1,2) );
+console.log( sumar2(1,2) );
