1a326b285f059d15dfe5e5df7e154dc731a0a299
duvan1026 <duvancastro1026@gmail.com>
2023-01-17 07:42:10 -0500

    if-else.js: se agrega ejercicio de estructura de control if-else para identificar el dia de la semana el cual nos encontramos

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -16,6 +16,21 @@ let dia = hoy.getDay(); // 0: Domingo, 1: lunes, 2: martes ......
 
 console.log({ dia });
 
+
+
 if( dia === 0 ) {
-    
+    console.log('Domingo');
+} else if( dia === 1 ) {
+
+    console.log('Lunes')
+   
+    // if( dia === 1 ){
+    //     console.log('Lunes');
+    // } else {
+    //     console.log('No es lunes ni domingo');
+    // }
+} else if( dia === 2 ) {
+    console.log('Martes');
+} else{
+    console.log(' No es lunes, martes o domingo ...')
 }
