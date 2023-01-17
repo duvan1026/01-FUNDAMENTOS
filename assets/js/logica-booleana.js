f6f3782f09826e42a06c180ca8abf9fb42632599
duvan1026 <duvancastro1026@gmail.com>
2023-01-17 16:37:33 -0500

    logica-booleana.js: Pro tip para asignaciones con operadores en JS

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -47,7 +47,29 @@ console.log('4 condiciones', true || true || true || false ); /// true
  
 
 
+console.warn( 'Asiganaciones' );
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola Mundo' && 150; // 150 ultimo valor
+const a2 = 'Hola' && 'Mundo' && soyFalso && true; // false
+
+// Asignaciones mas comunes.
+const a3 = soyFalso || 'Ya no soy falso'; // 'Ya no soy falso'
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true; // 'Ya no soy falso de nuevo', se ejecuta hasta cuando encuentra un valor y rompe el ciclo
+
+const a5 = soyFalso || soyUndefined || regresaTrue || 'Ya no soy falso de nuevo' || true; // 'true', se ejecuta hasta cuando encuentra un valor y rompe el ciclo
+
+console.log({ a1, a2, a3, a4 });
 
 
+
+if( regresaTrue()  && regresaFalse() && (true || true || true || false) ){ // No realizar de esta forma, ya que es mala practica
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
 
 
