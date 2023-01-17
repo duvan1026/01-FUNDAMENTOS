0c01efe87a70e617c54f8840517b0e22f7ceb10a
duvan1026 <duvancastro1026@gmail.com>
2023-01-17 08:32:29 -0500

    logica-booleana.js: se implementa el operador AND para logica booleana en JS

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -18,3 +18,24 @@ console.log( !false ); // true
 
 console.log( !regresaFalse() ); // true
 
+
+//***** ANd */
+console.warn( 'And' );// true si todos los valores son verdaderos
+console.log( true && true ); // true
+console.log( true && false ); // false
+console.log( true && !false ); // true
+
+console.log('=============='); // Separador
+console.log( regresaFalse()  && regresaTrue() ); // false
+console.log( regresaTrue()  && regresaFalse() ); // false
+
+console.log('=============='); // Separador
+regresaTrue()  && regresaFalse(); // Se ejecutan las funciones segun la logica, en este caso se ejecutan las dos porque la primera tiene como resultado un true
+regresaFalse()  &&  regresaTrue(); // Se ejecutan las funciones segun la logica, en este caso se ejecutan la priemra función porque da como resultado la primera un false
+
+
+
+
+
+
+
