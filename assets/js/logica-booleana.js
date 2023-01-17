82986c905b6607599f74d877316f7ae6702ff4b1
duvan1026 <duvancastro1026@gmail.com>
2023-01-17 16:23:16 -0500

    logica-booleana.js: se implementa el operador OR para logica booleana en JS

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -33,6 +33,18 @@ console.log('=============='); // Separador
 regresaTrue()  && regresaFalse(); // Se ejecutan las funciones segun la logica, en este caso se ejecutan las dos porque la primera tiene como resultado un true
 regresaFalse()  &&  regresaTrue(); // Se ejecutan las funciones segun la logica, en este caso se ejecutan la priemra función porque da como resultado la primera un false
 
+console.log('4 condiciones', true && true && true && false );
+
+
+//***** OR */
+console.warn('OR'); // true, almenos una condicion debe estar en true
+console.log( true || false ); // true
+console.log( false || false ); // false
+
+console.log( regresaTrue()  || regresaFalse() ); // Se ejecutan las funciones segun la logica, en este caso se ejecutan las primera porque es verdadero
+
+console.log('4 condiciones', true || true || true || false ); /// true
+ 
 
 
 
