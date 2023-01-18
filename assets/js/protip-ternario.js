17bbc0141d818223ab19110838e588dbe38a7e83
duvan1026 <duvancastro1026@gmail.com>
2023-01-18 11:09:16 -0500

    protip-ternario.js: integración de oeprador ternario en un arreglo en JS

--- assets/js/protip-ternario.js
+++ assets/js/protip-ternario.js
@@ -5,5 +5,22 @@
 //Reducción función anterior
 const elMayor = ( a,b ) => ( a > b ) ? a : b;
 
+const tieneMembresia = ( miembro ) => ( miembro ) ? '2 Dolares' : '10 Dolares';
+
 
 console.log( elMayor( 20, 15 ));
+console.log( tieneMembresia( false ) );
+
+
+//integración operador ternario en un arreglo
+const amigo = false;
+const amigosArr = [
+    'Peter',
+    'Tony',
+    'Dr. Strange',
+    amigo ? 'Thor' : 'Loki',
+    // (() => 'Nick Fury')(), // Funcion anonima autoinvocada
+    elMayor(10, 15),
+];
+
+console.log( amigosArr );
