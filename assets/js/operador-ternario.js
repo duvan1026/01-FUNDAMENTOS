e6721c2e6ce4556a3421fa7eec18deb697ccf449
duvan castro <duvancastro1026@gmail.com>
2023-01-17 21:01:26 -0500

    operador-ternario.js: ejercicio de control de apertura tienda resuelto con operador ternario

--- assets/js/operador-ternario.js
+++ assets/js/operador-ternario.js
@@ -10,18 +10,25 @@ const horaActual = 11;
 let horaApertura;
 let mensaje; // Esta Abierto, Está cerrado, hoy abrimos a las xx
 
-if ( dia === 0 || dia === 6 ){
-    console.log('Fin de semana');
-    horaApertura = 9;
-} else {
-    console.log('Día de semana ');
-    horaApertura = 11;
-}
+// if ( dia === 0 || dia === 6 ){
+// if ( [0,6].includes( dia ) ){ // se crea un arreglo con los dias de fin de semana y se aplica el metodo includes para preguntar si existe ese dia dentro del arreglo
+//     console.log('Fin de semana');
+//     horaApertura = 9;
+// } else {
+//     console.log('Día de semana ');
+//     horaApertura = 11;
+// }
+// Reducción del codigo anterior con operación ternaria
+horaApertura = ( [0,6].includes( dia ) ) ? 9 : 11;
 
-if ( horaActual >= horaApertura ){
-    mensaje = 'Está abierto';
-} else {
-    mensaje = `Esta cerrado, hoy abrimos a las ${ horaApertura }`;  // concatenación de string
-}
+// if ( horaActual >= horaApertura ){
+//     mensaje = 'Está abierto';
+// } else {
+//     mensaje = `Esta cerrado, hoy abrimos a las ${ horaApertura }`;  // concatenación de string
+// }
+
+//Reducción del codigo anterior con operador ternario.
+mensaje = ( horaActual >= horaApertura ) ? `Está abierto` : `Esta cerrado, hoy abrimos a las ${ horaApertura }`;
+
 
 console.log({ horaApertura, mensaje });
