c83bf73cb6323741de135ae259c84ad3f334855f
duvan1026 <duvancastro1026@gmail.com>
2023-01-19 11:58:40 -0500

    for.js: se implementa ejercicio del manejo del for tracional en JS

---
+++ assets/js/ciclos/for.js
@@ -0,0 +1,9 @@
+
+
+const heroes = ['Batman', 'Superman', 'Mujer Maravilla', 'Aquaman'];
+
+
+console.warn('For tradicional' );
+for( let i = 0; i < heroes.length ; i++ ) {
+    console.log( heroes[i] );
+}
