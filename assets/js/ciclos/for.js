db73fb1f959c0b4b6d5049991c602ec275122625
duvan1026 <duvancastro1026@gmail.com>
2023-01-19 12:03:31 -0500

    for.js: se implementa ejercicio del manejo del for in en JS

--- assets/js/ciclos/for.js
+++ assets/js/ciclos/for.js
@@ -7,3 +7,8 @@ console.warn('For tradicional' );
 for( let i = 0; i < heroes.length ; i++ ) {
     console.log( heroes[i] );
 }
+
+console.warn('For in' );
+for( let i in heroes ){
+    console.log( heroes[i] );
+}
