4b179d6c186acd811cfc4d6702582b95beade4bb
duvan1026 <duvancastro1026@gmail.com>
2023-01-19 11:50:24 -0500

    while-do-while.js: se implimenta el ciclo do while con sus respectivo ejemplo en JS

--- assets/js/ciclos/while-do-while.js
+++ assets/js/ciclos/while-do-while.js
@@ -5,6 +5,7 @@ const carros = ['Ford', 'Mazda', 'Honda', 'Toyota'];
 
 let i = 0;
 
+console.warn('While');
 // while( i < carros.length ) {
 //     console.log( carros[i] );
 //     i++;
@@ -25,3 +26,17 @@ while( carros[i]) {
     console.log( carros[i] );
     i++;
 }
+
+
+
+console.warn('Do While');
+
+let j = 0;
+
+do {
+    console.log( carros[j] );
+    j++;
+
+} while( carros[j] );
+
+ 
