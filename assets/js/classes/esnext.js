f42445eec4dd3f50fd726faecd1fd5e23a1fbf81
duvan1026 <duvancastro1026@gmail.com>
2023-02-02 15:36:04 -0500

    esnext.js: se implementa propiedades privadas en una clase en JS

---
+++ assets/js/classes/esnext.js
@@ -0,0 +1,20 @@
+
+
+
+class Rectangulo {
+
+    #area = 0; // Variable privada
+
+    constructor( base = 0, altura = 0 ) {
+        this.base = base;
+        this.altura = altura;
+
+
+        this.#area = base * altura;
+    }
+}
+
+const rectangulo = new Rectangulo( 10, 15 );
+// rectangulo.#area = 100;
+
+console.log( rectangulo );
